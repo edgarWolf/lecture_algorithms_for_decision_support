57613d10fa0b189e236a4717f5b4ce2af1d18c9a
edgar <edgar9.wolf.8@gmail.com>
2021-12-16 18:04:18 +0100

    Updated solution for exercise 4.

--- Übung 4/uebung4.R
+++ Übung 4/uebung4.R
@@ -30,7 +30,16 @@ phaseDiagram2S(A, Replicator, strategies = c("a1", "a2"))
 
 # 2.)
 
-(A <- matrix(c(0,0,1,2), 2, byrow=T))
+(A <- matrix(c(1,2,0,0), 2, byrow=T))
 stratgies <- c("a1", "a2")
 ESS(A, strategies)
 phaseDiagram2S(A, Replicator, strategies = c("a1", "a2"))
+
+
+
+# 5.)
+# Refers to: rock-paper-scissor (according to the matrix provided below in this order) 
+A <- matrix(c(0, -1, 1, 1, 0, -1, -1, 1, 0), 3, byrow=T)
+state <- matrix(c(0.4, 0.3, 0.3), 1, 3, byrow=T)
+# state <- matrix(c(1/3, 1/3, 1/3), 1, 3, byrow=T)
+phaseDiagram3S(A, Replicator, NULL, state, FALSE, TRUE)
